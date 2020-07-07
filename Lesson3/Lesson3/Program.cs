cbd24a143ffe3426df011ca51592e5a28ac19e51
LanFun1 <66485374+LanFun1@users.noreply.github.com>
2020-07-07 19:22:34 +0300

    Add files via upload

---
+++ Lesson3/Lesson3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int crystals = 0;
+            int price = 7;
+
+            Console.WriteLine("Скажите, сколько у вас есть золота?");
+            gold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вы можете купить " + (gold / price) + " кристаллов");
+            
+            while(crystals == 0)
+            {
+                int crystalsAmount = Convert.ToInt32(Console.ReadLine());
+                if(crystalsAmount*price>gold)
+                {
+                    Console.WriteLine("У вас недостаточно денег. Выберите меньшее количество кристаллов.");
+                }
+                else
+                {
+                    crystals = crystalsAmount;
+                    gold = gold - (price* crystalsAmount);
+                }
+            }
+
+            Console.WriteLine("Gold = " + gold + "\ncrystals = " + crystals);
+            Console.ReadKey();
+        }
+    }
+}
